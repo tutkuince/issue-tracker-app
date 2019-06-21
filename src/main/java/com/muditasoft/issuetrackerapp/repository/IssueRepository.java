428615bad7293200fdb37a183534b2c713eac85f
tutkuince <tutku.ince@outlook.com>
2019-06-21 21:59:16 +0300

    CrudRepository to JpaRepository

--- src/main/java/com/muditasoft/issuetrackerapp/repository/IssueRepository.java
+++ src/main/java/com/muditasoft/issuetrackerapp/repository/IssueRepository.java
@@ -1,8 +1,8 @@
 package com.muditasoft.issuetrackerapp.repository;
 
 import com.muditasoft.issuetrackerapp.model.Issue;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IssueRepository extends CrudRepository<Issue, Long> {
+public interface IssueRepository extends JpaRepository<Issue, Long> {
 
 }

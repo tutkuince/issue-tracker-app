1e8845657d9dbccea78d5f38953305c9f64dda57
tutkuince <tutku.ince@outlook.com>
2019-06-21 21:22:26 +0300

    ModelMapper dependency is injected

--- src/main/java/com/muditasoft/issuetrackerapp/IssueTrackerAppApplication.java
+++ src/main/java/com/muditasoft/issuetrackerapp/IssueTrackerAppApplication.java
@@ -1,13 +1,24 @@
 package com.muditasoft.issuetrackerapp;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class IssueTrackerAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IssueTrackerAppApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
     }
 
 }

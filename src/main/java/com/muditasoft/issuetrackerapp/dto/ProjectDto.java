e38ac68983a087feef4f272c7793d4dfd6fd6ce1
tutkuince <tutku.ince@outlook.com>
2019-06-21 22:32:27 +0300

    Null check feature is added to projectName and projectCode fields

--- src/main/java/com/muditasoft/issuetrackerapp/dto/ProjectDto.java
+++ src/main/java/com/muditasoft/issuetrackerapp/dto/ProjectDto.java
@@ -2,10 +2,16 @@ package com.muditasoft.issuetrackerapp.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class ProjectDto {
     private Long id;
+
+    @NotNull
     private String projectName;
+
+    @NotNull
     private String projectCode;
     private UserDto userDto;
 }

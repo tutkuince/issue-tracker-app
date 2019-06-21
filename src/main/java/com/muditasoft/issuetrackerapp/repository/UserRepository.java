f6fe23fc015948c9a9fe94a4fbeff8f85edab20c
tutkuince <tutku.ince@outlook.com>
2019-06-21 22:01:19 +0300

    CrudRepository to JpaRepository

--- src/main/java/com/muditasoft/issuetrackerapp/repository/UserRepository.java
+++ src/main/java/com/muditasoft/issuetrackerapp/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package com.muditasoft.issuetrackerapp.repository;
 
 import com.muditasoft.issuetrackerapp.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
